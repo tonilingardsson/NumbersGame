dbb90366103523ee9986c3996901f704dd0be653
tonilingardsson <toni.lingardsson@gmail.com>
2025-09-19 16:36:24 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace NumbersGame
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+           System.Console.WriteLine("Hello World from NumbersGame!");
+        }
+    }    
+}
